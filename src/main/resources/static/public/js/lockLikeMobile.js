ab124abe62876d47b9c4baba3879696088ff64c5
qchod <qchod@naver.com>
2025-11-26 16:30:55 +0900

    마이페이지, lockLikeMobile 수정

--- src/main/resources/static/public/js/lockLikeMobile.js
+++ src/main/resources/static/public/js/lockLikeMobile.js
@@ -1,8 +1,6 @@
 // 가장 큰 문제 사항: 크기와 마진을 조절 가능하게 할 시에 fixed top, fixed bottom이 조절 가능 할 것인가
 // 현실적: 그냥 그대로 fixed top, fixed bottom 최상단 하단에 놓고... 크기 조절 -> 문제점: body 사이즈 변화에 대응해야 하나?
 
-// 확인된 문제: absolute 문제 - 충분히 모아 볼 것 (은비)
-
 // 주요 세팅값
 const llmSettingValues = {
     autoHeight: true,                           // 높이 최대 크기
@@ -10,7 +8,7 @@ const llmSettingValues = {
     fixedWidthRatio: 9,                         // 너비 배율
     commonRatio: 3.3,                           // 크기 배율
     wrapperBackgroundColor: "#FFFFFF",          // 가두는 상자 배경 색
-    bodyBackgroundColor: "#EEEEEE",             // body 배경 색
+    bodyBackgroundColor: "#f6f6f6",             // body 배경 색
     wrapperTopMargin: 0,                        // wrapper top margin
     wrapperScrollWidth: 'none',                 // wrapper 스크롤 너비: ['none' | 'thin' | 'initial']
     anotherScrollWidth: 'thin',                 // 그 외 스크롤 너비: ['none' | 'thin' | 'initial'] // thin이 가장 적당 할 듯
@@ -20,69 +18,22 @@ const llmSettingValues = {
 const llmLeftSummarySettingValues = {
     left: 3,                                    // auto 옵션 만들어야 되나
     top: 3,
-    backgroundColor: "#FFF",
+    // backgroundColor: "#FFF",
     width: 25,
     height: 53,
 };
 
 // 오른쪽 요약 페이지 세팅값(em)
 const llmRightSummarySettingValues = {
-    right: 0,                                    // auto 옵션 만들어야 되나
-    top: 0,
+    right: 3,                                    // auto 옵션 만들어야 되나
+    top: 3,
     // backgroundColor: "#FFF",
-    width: 0,
+    width: 15,
     height: 50,
 };
 
 
 const leftSummaryHtml = `
-<div class="container">
-    <div class="row align-items-center">
-        <div class="col">
-             <div class="row justify-content-center">
-                <img class="col-auto mt-5 pt-5 img-fluid" src="/public/img/common/배캠.png" style="width:80%; height:80%">
-            </div>
-            <div class="row mt-4">
-                <div class="col text-center">Basecamp는 <br> 모바일 환경에 최적화 되어있습니다.</div>
-            </div>
-            <div class="row justify-content-center my-3">
-                <img class="col-auto img-fluid" src="/public/img/common/qrCode.png" style="width: 50%; height: 50%">
-            </div>
-            <div class="row">
-                <div class="col text-center">모바일 환경으로 로그인하기</div>
-            </div>
-            <div class="row mt-5">
-                <a href="/seller/login" class="col text-center lightFont fw-semibold">판매자 페이지</a>
-            </div>
-            <div class="row pt-3">
-                <div class="col new-fs-85 text-center">
-                    <span class="text-secondary">비밀번호 : 1234</span>
-                </div>
-            </div>
-             <div class="row pt-1 mt-5">
-                <div class="col fw-semibold text-center">
-                    캠핑장 쓰레기투기 방지 캠페인
-                </div>
-            </div>
-            <a class="row mt-2 justify-content-center"  href="/camp/unity">
-                <div class="col-auto">
-                    <img class="img-fluid" src="/unity/CampgameQR.png" style="height:9em;">
-                </div>
-            </a>
-             <div class="row flex-column mt-2">
-                <div class="col text-center">
-                    ↑캠프파이어 게임 플레이하기↑
-                </div>
-                 <div class="col text-center">
-                    QR코드를 클릭하세요!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-`;
-
-const rightSummaryHtml = `
 <div class="row">
     <div class="col">
      
@@ -90,10 +41,48 @@ const rightSummaryHtml = `
 </div>
 `;
 
+const rightSummaryHtml = `
+<div class="container">
+    <div class="row align-items-start">
+        <div class="col py-3 rounded-1 fw-semibold" style="background-color: #FFFFFF; align-items: center">
+<!--             <div class="row justify-content-start">-->
+<!--                <img class="col-auto mt-5 pt-5 img-fluid" src="/public/img/common/배캠.png" style="width:50%; height:50%">-->
+<!--            </div>-->
+            <div class="row justify-content-start">
+                <img class="col-auto img-fluid px-3" src="/public/img/common/qrCode.png" style="width: 50%; height:50%">
+            </div>
+            <div class="row mt-2">
+                <div class="col text-start px-3">Basecamp는 <br> 모바일 환경에 <br> 최적화 되어있습니다.</div>
+            </div>
+            <div class="row mt-2 text-body-tertiary f08">
+                <div class="col text-start px-3">QR코드를 스캔해보세요</div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col py-3 rounded-1" style="background-color: #FFFFFF">
+            <div class="row">
+                <a href="/seller/login" class="col text-start fw-semibold px-3">판매자 페이지 바로가기</a>
+            </div>
+            <div class="row pt-2 f08">
+                <div class="col new-fs-85 text-start px-3">
+                    <span class="text-secondary">비밀번호 : 1234</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <a class="col fw-semibold form-control text-center py-3 btn bg-black text-white rounded-1 f09" href="/camp/unity">
+            캠프파이어 게임 플레이하기
+        </a>
+    </div>
+</div>
+`;
+
 
 
 window.addEventListener("DOMContentLoaded", () => {
-      // 모바일인 경우
+    // 모바일인 경우
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         return;
     }
@@ -120,18 +109,43 @@ function createLeftSummary() {
 
 }
 
+// function createRightSummary() {
+//     const summaryWrapper = document.createElement("div");
+//     summaryWrapper.style.position = "absolute";
+//     summaryWrapper.style.right = `${llmRightSummarySettingValues.right}em`;
+//     summaryWrapper.style.top = `${llmRightSummarySettingValues.top}em`;
+//     summaryWrapper.style.backgroundColor = llmRightSummarySettingValues.backgroundColor;
+//     summaryWrapper.style.width = `${llmRightSummarySettingValues.width}em`;
+//     summaryWrapper.style.height = `${llmRightSummarySettingValues.height}em`;
+//
+//     summaryWrapper.innerHTML = rightSummaryHtml;
+//
+//     document.body.appendChild(summaryWrapper);
+// }
 function createRightSummary() {
     const summaryWrapper = document.createElement("div");
     summaryWrapper.style.position = "absolute";
-    summaryWrapper.style.right = `${llmRightSummarySettingValues.right}em`;
-    summaryWrapper.style.top = `${llmRightSummarySettingValues.top}em`;
     summaryWrapper.style.backgroundColor = llmRightSummarySettingValues.backgroundColor;
     summaryWrapper.style.width = `${llmRightSummarySettingValues.width}em`;
     summaryWrapper.style.height = `${llmRightSummarySettingValues.height}em`;
-
     summaryWrapper.innerHTML = rightSummaryHtml;
 
     document.body.appendChild(summaryWrapper);
+
+    // 위치 갱신 함수 정의
+    const updateRightSummaryPosition = () => {
+        const mobileWrapper = document.querySelector("body > div");
+        const rect = mobileWrapper.getBoundingClientRect();
+
+        summaryWrapper.style.left = `${rect.right + 50}px`;
+        summaryWrapper.style.top = `${window.innerHeight * 0.8 - (summaryWrapper.offsetHeight / 2)}px`;
+    };
+
+    // 초기 위치 설정
+    updateRightSummaryPosition();
+
+    // 창 크기 변경 시마다 재계산
+    window.addEventListener("resize", updateRightSummaryPosition);
 }
 
 
@@ -194,15 +208,11 @@ function lockFixedDiv(targetElement, width) {
     ){
 
         targetElement.style.width = width;
-        // targetElement.style.margin = "0 auto"; // 민지가 찾아냄... 난 이게 왜 문제인지 전혀 모르겠다, 현영 - 오히려 이게 있는게 더 잘 맞음
-
-        // 민지 - 모달에 left 속성 원래 있나??
         targetElement.style.left = "auto";
         targetElement.style.right = "auto";
 
     }
 
-    // 재귀
     for(element of targetElement.children){
         lockFixedDiv(element, width);
     }
@@ -212,7 +222,6 @@ function setScrollWidth(targetElement, width){
 
     targetElement.style.scrollbarWidth = width;
 
-    // 재귀
     for(element of targetElement.children){
         setScrollWidth(element, width);
     }
